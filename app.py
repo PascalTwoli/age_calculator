e681b4c5e4c6d1c60dd396965d8e80f6e0112070
Pascal Twoli <pascaltwoli@gmail.com>
2025-02-26 12:07:48 +0300

    updated app.py

--- app.py
+++ app.py
@@ -2,9 +2,13 @@ from flask import Flask, render_template, request, jsonify
 from flask_cors import CORS
 from datetime import date
 from dateutil.relativedelta import relativedelta
+import os
+
 
 app = Flask(__name__)
 CORS(app)
+port = int(os.environ.get("PORT", 5000))
+app.run(host="0.0.0.0", port=port)
 
 # Route to serve the html page
 @app.route("/")
