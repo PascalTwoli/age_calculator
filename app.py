41dbf40dc59e6dc2fed31ec6d560a876907a538c
Pascal Twoli <pascaltwoli@gmail.com>
2025-02-26 12:50:34 +0300

    updated app.py

--- app.py
+++ app.py
@@ -6,17 +6,18 @@ import os
 
 
 app = Flask(__name__)
-CORS(app, supports_credentials=True)
+CORS(app, resources={r"/age_calculations": {"origins": "*"}})
 port = int(os.environ.get("PORT", 5000))
 app.run(host="0.0.0.0", port=port)
 
 # Route to serve the html page
 @app.route("/")
 def home(): 
-    return render_template("index.html")
+    # return render_template("index.html")
+    return "API is working!"
 
 # API to calculate age
-@app.route("/age_calculations", methods=["POST"])
+@app.route("/age_calculations", methods=["POST", "OPTIONS"])
 def age_calculations(): 
     try:
         if request.method == "OPTIONS":
@@ -42,4 +43,5 @@ def age_calculations():
         }), 400
     
 if __name__ == "__main__":
-    app.run(debug=True)
+    # app.run(debug=True)
+    app.run(host="0.0.0.0", port=5000)
